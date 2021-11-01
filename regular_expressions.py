9e52170538d9b04ccf09f64fcc7207b291700ec0
S.Razavi <verdaura6@gmail.com>
2021-11-01 16:12:05 +0330

    taking about regular expressions

---
+++ regular_expressions.py
@@ -0,0 +1,6 @@
+"""
+    * regular expression allow au to search for patterns in python strings.
+    * they can seem incredibly intimidating at first due to their strange syntax!
+    * we'll walk through the basics of regular expressions, we will use them in django!
+"""
+
