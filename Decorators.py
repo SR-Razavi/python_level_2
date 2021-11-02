6e33738f58591358fa9bf008e25ad00b8438751f
S.Razavi <verdaura6@gmail.com>
2021-11-02 17:02:38 +0330

    about decorator

---
+++ Decorators.py
@@ -0,0 +1,35 @@
+
+
+def new_decorator(func):
+
+    def wrap_func():
+        print("code here befor executing func")
+        func()
+        print('func() has been called')
+    return wrap_func
+
+def func_needs_decorator():
+    print('this function is in need of a decorator!')
+
+
+new_decorator(func_needs_decorator)()
+
+print('------------------------------ b')
+
+func_needs_decorator = new_decorator(func_needs_decorator)
+func_needs_decorator()
+
+
+print('------------------------------ e')
+
+
+
+func_needs_decorator = new_decorator(func_needs_decorator)
+func_needs_decorator()
+
+print('------------------------------ b')
+
+new_decorator(func_needs_decorator)()
+
+
+
