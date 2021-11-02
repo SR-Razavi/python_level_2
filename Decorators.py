aee07e05f6fa5a4c63e0d3bb43dfa69d6ca020a7
S.Razavi <verdaura6@gmail.com>
2021-11-02 17:04:47 +0330

    use @ for decorator

--- Decorators.py
+++ Decorators.py
@@ -8,28 +8,30 @@ def new_decorator(func):
         print('func() has been called')
     return wrap_func
 
+
+@new_decorator
 def func_needs_decorator():
     print('this function is in need of a decorator!')
 
 
-new_decorator(func_needs_decorator)()
+# new_decorator(func_needs_decorator)()
 
-print('------------------------------ b')
+# print('------------------------------ b')
 
-func_needs_decorator = new_decorator(func_needs_decorator)
-func_needs_decorator()
-
-
-print('------------------------------ e')
+# func_needs_decorator = new_decorator(func_needs_decorator)
+# func_needs_decorator()
 
 
+# print('------------------------------ e')
 
-func_needs_decorator = new_decorator(func_needs_decorator)
-func_needs_decorator()
 
-print('------------------------------ b')
 
-new_decorator(func_needs_decorator)()
+# func_needs_decorator = new_decorator(func_needs_decorator)
+# func_needs_decorator()
+
+# print('------------------------------ b')
+
+# new_decorator(func_needs_decorator)()
 
 
 
