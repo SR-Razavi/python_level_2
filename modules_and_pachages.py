178fb7929370ad44ec60c624d3eb61b5d547acb7
S.Razavi <verdaura6@gmail.com>
2021-11-02 11:15:45 +0330

    init module and package in python

---
+++ modules_and_pachages.py
@@ -0,0 +1,7 @@
+"""
+    * Module and Package.
+    * you have seen puthon import statments, but have probably wondered, how do they work
+        and how do we create our own?
+    
+    * in this lecture you learn how can create you'r own module and then import it into another file.
+"""
